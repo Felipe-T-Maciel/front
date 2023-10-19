ef5b5030d2011b6e7bd0cea1408731017868a7e7
Felipe Tomio Maciel <felipetomio@weg.net>
2023-10-18 21:54:22 -0300

    first commit

---
+++ component/bdComponent.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const fetchProdutos = async () => {
+  try {
+    const response = await axios.get('http://10.4.96.59:8082/users/todos');
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar produtos:", error);
+  }
+};
+
+export default function MyComponent(){
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const result = await fetchProdutos();
+      console.log(result)
+      setUsers(result);
+    };
+
+    loadData();
+  }, []);
+
+  return (
+    <>
+        <div className='h-screen w-full bg-red-600'>
+            {users.map(user => (
+                <div className='text-black' key={user.id}>Usuario: {user.nome}</div>
+            ))}
+        </div>
+    </>
+  );
+};
+
+
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import MyComponent from '../../component/bdComponent.jsx'
+
+export default function Home() {
+  return (
+    <>
+      <div className='p-10'>
+        <MyComponent/>
+      </div>
+    </>
+  )
+}
